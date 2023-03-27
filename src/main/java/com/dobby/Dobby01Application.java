b48aa0e3deadd17af65a1c118efb2a3d621d22eb
박주성 <juseong001214@gmail.com>
2023-03-27 20:46:26 +0900

    초기설정

---
+++ src/main/java/com/dobby/Dobby01Application.java
@@ -0,0 +1,13 @@
+package com.dobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dobby01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dobby01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dobby/Dobby01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dobby01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
